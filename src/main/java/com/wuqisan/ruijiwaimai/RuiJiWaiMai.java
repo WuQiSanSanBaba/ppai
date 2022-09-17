bf14d77d92f84cb1284656c81e5138e34b6fb175
WuQiSanSanBaba <931441227@qq.com>
2022-09-17 23:13:03 +0800

    tijiaobei

--- src/main/java/com/wuqisan/ruijiwaimai/RuiJiWaiMai.java
+++ src/main/java/com/wuqisan/ruijiwaimai/RuiJiWaiMai.java
@@ -3,8 +3,11 @@ package com.wuqisan.ruijiwaimai;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class RuiJiWaiMai {
     public static void main(String[] args) {
         SpringApplication.run(RuiJiWaiMai.class);
